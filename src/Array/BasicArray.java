8130dda8d630664b439cb197214575de6e6970b2
Prakash Thakur <jay.prakash.thakur@accenture.com>
2016-12-19 15:50:53 +0530

    Initial commit

---
+++ src/Array/BasicArray.java
@@ -0,0 +1,16 @@
+package Array;
+
+/**
+ * Created by jay.prakash.thakur on 19-12-2016.
+ */
+public class BasicArray {
+    public static void main(String[] args) {
+        int[] arr = {10,20,30,40,50};
+
+        //printing array elements
+        for (int temp:arr) {
+            System.out.print(temp+" ");
+        }
+
+    }
+}
---
+++ src/LinkedList/Methods.java
@@ -0,0 +1,7 @@
+package LinkedList;
+
+/**
+ * Created by jay.prakash.thakur on 19-12-2016.
+ */
+public class Methods {
+}
