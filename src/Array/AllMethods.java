0123bfcd4af68e849905120d391ddc4fd5ecb9ec
jay thakur <jayprakashthakursnr@gmail.com>
2016-12-20 12:09:30 +0530

    Insert operation in an unsorted array implementation.

--- src/Array/AllMethods.java
+++ src/Array/AllMethods.java
@@ -8,11 +8,30 @@ class AllMethods {
      * @return an int/-1
      */
     int findElementInUnsortedArray(int[] arr, int key){
-        for (int i=0; i<arr.length; i++){
+        for (int i = 0; i < arr.length; i++){
             if (arr[i] == key){
                 return i;
             }
         }
         return -1;
+    }
+
+    /**
+     * @param arr array of integers
+     * @param key value to insert
+     * @return arr
+     */
+    int[] insertInUnsortedArray(int[] arr, int key){
+        arr[5] = key;
+        return arr;
+    }
+
+    /**
+     * @param arr to print
+     */
+    void printArray(int[] arr){
+        for (int temp:arr) {
+            System.out.print(temp+" ");
+        }
     }
 }
--- src/Array/OperationInAnUnsortedArray.java
+++ src/Array/OperationInAnUnsortedArray.java
@@ -5,16 +5,33 @@ package Array;
  */
 public class OperationInAnUnsortedArray {
 
+    static AllMethods allMethods = new AllMethods();
+
     public static void main(String[] args) {
-        int[] arr = {2, 24, 100, 6, 44};
-        int key = 6;
-        int keyPosition = new AllMethods().findElementInUnsortedArray(arr, key);
+        int[] arr = new int[6];
+        arr[0] = 2;
+        arr[1] = 24;
+        arr[2] = 100;
+        arr[3] = 6;
+        arr[4] = 44;
+
+        System.out.println("array size is : "+arr.length);
+
+        System.out.println("********** Search Implementation **********");
+
+        int key = 10;
+        int keyPosition = allMethods.findElementInUnsortedArray(arr, key);
 
         if (keyPosition == -1){
             System.out.println("Element not found");
         }else{
             System.out.println("Element found at position : "+(keyPosition+1));
         }
+
+        System.out.println("---------- Insert Implementation ----------");
+        arr = allMethods.insertInUnsortedArray(arr,50);
+        System.out.println("-- After Inserting --");
+        allMethods.printArray(arr);
 
     }
 }
